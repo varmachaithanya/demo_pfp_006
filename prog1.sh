a910c51e4ac09ed65e79bdc12263bae1c494e053
chaithanya <varma@gmail.com>
2023-11-15 13:23:47 +0530

    added multiplication

--- prog1.sh
+++ prog1.sh
@@ -1,4 +1,28 @@
-a=10
-b=20
-sum=$(($a+$b))
-echo sum is $sum
+echo "Welcome to the Calculator"
+echo "enter 1st number"
+read a
+echo "enter 2nd number"
+read b
+
+echo "enter ypur choice"
+echo "enter 1: for addition"
+echo "enter 2: for subtraction"
+echo "enter 3: for multiplication"
+read input
+case $input in
+	1)
+		sum=$(($a+$b))
+		echo sum is $sum
+		;;
+	2)
+		sub=$(($a-$b))
+		echo sub is $sub
+		;;
+	3)
+		mul=$(($a*$b))
+		echo mul is $mul
+		;;
+	*)
+		echo Incorrect Information 
+		;;
+esac
